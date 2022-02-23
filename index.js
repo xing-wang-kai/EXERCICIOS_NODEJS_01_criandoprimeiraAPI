bd774aba646e5716711bce4c71465118d6eb6233
Kai Wang <kaiuchihaa@gmail.com>
2022-02-22 21:48:24 -0300

    novos exercicios
    usando função them catch para promises

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
 const fs = require('fs');
 
-function passarError(error){
+
+
+
+ function passarError(error){
     throw new Error(error.code, '..dnt found the path File')
 }
-function pegaArquivo(arquivo){
+function pegarArquivo(arquivo){
+    const parametros = 'utf-8'
+    fs.promises
+    .readFile(arquivo, parametros)
+    .then((texto) => {console.log(texto)})
+    .catch((error) => {passarError(error)})
+}
+/**function pegaArquivo(arquivo){
     const parametro = 'utf-8'
     fs.readFile(arquivo, parametro, (error, texto) => {
        if(error){
@@ -16,5 +26,6 @@ function pegaArquivo(arquivo){
     } )
 
 }
+ */
+pegarArquivo('./Arquivos/texto1.md')
 
-pegaArquivo('./Arquivos/texto1.md')
