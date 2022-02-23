f67e1d58598e39235bf2e4c10f199bd2e2f4028c
Kai Wang <kaiuchihaa@gmail.com>
2022-02-23 00:10:34 -0300

    novos exercicios
    novos exercicios

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EXERCICIOS_NODEJS-CriandoAPI
  Criando a Primeira API com nodejs
+
+
+usando tipos com .this .catch async e await para promises
--- index.js
+++ index.js
@@ -2,19 +2,31 @@
 const fs = require('fs');
 
 
-
-
  function passarError(error){
     throw new Error(error.code, '..dnt found the path File')
 }
-function pegarArquivo(arquivo){
+
+async function pegarArquivo(arquivo){
+    const charset = 'utf-8';
+    try{
+        const texto = await fs.promises.readFile(arquivo, charset)
+        console.log(texto)
+    }catch(error){
+        passarError(error);
+    }
+}
+/**
+ * method usando o tipo de .then .catch
+ * function pegarArquivo(arquivo){
     const parametros = 'utf-8'
     fs.promises
     .readFile(arquivo, parametros)
     .then((texto) => {console.log(texto)})
     .catch((error) => {passarError(error)})
 }
-/**function pegaArquivo(arquivo){
+
+metodo comum
+function pegaArquivo(arquivo){
     const parametro = 'utf-8'
     fs.readFile(arquivo, parametro, (error, texto) => {
        if(error){
